92d9e560986ff4533a8f9a60e710cd9fff321bdb
Feather <lisafeather@gmail.com>
2013-09-20 14:19:43 -0700

    add code to stub

---
+++ while.rb
@@ -0,0 +1,5 @@
+puts "what is 2 to the 16th power?"
+answer = gets.chomp.to_i
+
+# Use a while loop to let the user guess again until he/she gets the answer right.
+# Once the user guesses right, print "Good Job"
